7466e5f71eb55a6ccd5a099e17318d44bbb982e0
mahesh <banagarmahesh2002@gmail.com>
2023-05-10 15:56:35 +0530

    docker

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def predict():
     return render_template('home.html',prediction_text="The House Price Prediction is {}".format(output))
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=int("3000"))
